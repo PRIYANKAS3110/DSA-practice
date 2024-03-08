570427b2ba7d240f8a9013e4fe04d4e2afa405a6
priyankas3110 <priyankasekarjaya@gmail.com>
2024-03-08 21:22:33 +0530

    Inverted Right Pyramid

---
+++ pattern/Inverted Right Pyramid.java
@@ -0,0 +1,26 @@
+package pattern;
+
+class Main {
+    static void pattern5(int N)
+ {
+     
+     for (int i = 0; i < N; i++)
+     {
+        
+         for (int j = N; j > i; j--)
+         {
+             System.out.print("* ");
+         }
+ 
+         
+         System.out.println();
+     }
+ }
+ 
+     public static void main(String[] args) {
+         
+   
+         int N = 5;
+         pattern5(N);
+     }
+ }
