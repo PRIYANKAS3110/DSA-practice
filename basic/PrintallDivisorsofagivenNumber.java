61e014a1fb9cbd4fa553077aa00987b85d00b635
priyankas3110 <priyankasekarjaya@gmail.com>
2024-03-09 16:24:28 +0530

    Print all Divisors of a given Number

---
+++ basic/PrintallDivisorsofagivenNumber.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class PrintallDivisorsofagivenNumber{
+		
+	public static void main(String[] args){
+		
+		printDivisorsBruteForce(36);
+		
+	}
+
+	static void printDivisorsBruteForce(int n){
+		System.out.println("The Divisors of "+n+" are:");
+		for(int i = 1; i <= n; i++)
+			if(n % i == 0)
+				System.out.print(i + " ");
+			
+		System.out.println();
+	}
+	
+}
